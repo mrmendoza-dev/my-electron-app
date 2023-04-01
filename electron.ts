ba885282c620d16073f0b7beb763a54f3102da64
Steven Mendoza <smendoza171@gmail.com>
2023-04-01 07:32:56 -0700

    first commit

---
+++ electron.ts
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+    },
+  });
+
+  // Load the React app.
+  win.loadURL("http://localhost:5173");
+
+  // Open the DevTools.
+//   win.webContents.openDevTools();
+}
+
+// When the app is ready, create the window.
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
